ce1d8807c1e7737a33344459b8fdbc0a28022092
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-02-14 12:23:47 -0300

    Add class.js file

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import { navSection } from "./modules/navbar.js";
+import { BookCollection } from "./modules/class.js";
 
 navSection('list-nav');
-
 const navList = ["list-nav", "add-nav", "contact-nav"];
-
 navList.forEach(element => {
   let display = document.getElementById(`${element}`);
   display.addEventListener("click", () => {
     navSection(element);
   })
+});
+
+const btnAdd = document.getElementById('btn-add-id');
+const runClass = new BookCollection();
+runClass.displayBooks();
+
+btnAdd.addEventListener('click', () => {
+  runClass.addBooks();
+  runClass.displayBooks();
 });
